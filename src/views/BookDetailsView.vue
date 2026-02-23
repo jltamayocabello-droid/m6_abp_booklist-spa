191b5305edae038b2b88d9b611a302cf3df13677
Jorge <jltamayocabello@gmail.com>
2026-02-23 13:10:23 -0300

    Se crea BookDetailsView.vue

---
+++ src/views/BookDetailsView.vue
@@ -0,0 +1,18 @@
+
+
+<template>
+  <section>
+    <h1>Book Details</h1>
+    <p>Book id: {{ props.id }}</p>
+
+    <!-- Lección 5: aquí buscarás el libro por id y mostrarás su info -->
+  </section>
+</template>
+
+<script setup>
+const props = defineProps({
+  id: { type: String, required: true }
+})
+</script>
+
+
--- src/views/ListaLibros.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div>
-    <h1>Lista de Libros</h1>
-  </div>
-</template>
-
-<style scoped></style>
-
-
