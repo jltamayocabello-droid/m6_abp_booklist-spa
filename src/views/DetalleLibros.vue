7d8d11d71ab11ac02cf2335611dcb493df731afa
Jorge <jltamayocabello@gmail.com>
2026-02-23 00:30:57 -0300

    Se crea la estructura de carpetas en components, views, router y assets

---
+++ src/views/DetalleLibros.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>
+    <h1>DetalleLibros</h1>
+  </div>
+</template>
+
+<style scoped></style>
---
+++ src/views/ListaLibros.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <h1>Lista de Libros</h1>
+  </div>
+</template>
+
+<style scoped></style>
+
+
