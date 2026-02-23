81440ffd655fb6b0f3df26280b01d7b1f0cadcb3
Jorge <jltamayocabello@gmail.com>
2026-02-23 12:50:45 -0300

    Se incorporan import de HomeView, BookView y BookDetailsView en index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,10 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
+import HomeView from '../views/HomeView.vue'
+import BooksView from '../views/BooksView.vue'
+import BookDetailsView from '../views/BookDetailsView.vue'
+
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [],
